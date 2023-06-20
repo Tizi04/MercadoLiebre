4c0f874f683b46952c12f9da26286b59b7847ea3
Tizi04 <tiziluca@hotmail.com>
2023-06-20 14:43:39 -0400

    MercadoLiebre

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+const publicPath = path.join(__dirname, "/public");
+
+const port = process.env.PORT || 3030;
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.listen(port, () => {
+  console.log(`Conectado en el puerto ${port}`);
+})
